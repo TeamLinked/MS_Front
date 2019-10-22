4708a5ad733d07997bfeb638af84be6394901b99
Ivan <irdelgadog@unal.edu.co>
2019-10-22 01:57:26 -0500

    Update Header

--- teamlinked/src/components/App.js
+++ teamlinked/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../assets/logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Estilos de React Boostrap
 import '../styles/App.css'; //Estilos
 
 
@@ -7,25 +9,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ana:''
       }
   }
+  
   render() { 
     return (  
       <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer" 
-        >
-          Learn React
-        </a>
+        <a>Learn React</a>
       </header>
     </div>
     );
