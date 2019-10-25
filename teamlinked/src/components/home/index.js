62e152fc06d8730f4a206a7b3f344f8489bd2762
Juan D. Gaitan <jdgaitanf@unal.edu.co>
2019-10-25 18:13:18 -0500

    Home Fase Inicial

--- teamlinked/src/components/home/index.js
+++ teamlinked/src/components/home/index.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Foro from '../foros/Foro'
+import  Foro from '../foros/Foro'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/scss/paper-dashboard.scss?v=1.1.0";
+import "./assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import Feed from "./Feed"
 
 class Home extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            foros: []
-        }
+        // this.state = {
+        //     foros: []
+        // }
     }
 
-    componentDidMount() {
-        axios.get(`http://34.94.59.230:3050/graphql`,
-            { 
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }}
-        )    
-          .then(res => {
-            const foros = res.data;
-            // console.log(res.data);
-            this.setState({ foros });
-        })
-    }
+    // componentDidMount()  {
+    //     axios.get(`http://34.94.59.230:3050/graphql`,
+    //         { 
+    //         headers: {
+    //             'Access-Control-Allow-Origin': '*'
+    //         }}
+    //     )    
+    //       .then(res => {
+    //         const foros = res.data;
+    //         // console.log(res.data);
+    //         this.setState({ foros });
+    //     })
+    // }
 
     render() {
         return (
             <div>
-                { this.state.foros.map(foro => <Foro key={foro.id} foro={foro} />)}
+                {/* { this.state.foros.map(foro => <Foro key={foro.id} foro={foro} />)} */}
+                <Feed/>
             </div>
+            
+            
         )
     }
 }
