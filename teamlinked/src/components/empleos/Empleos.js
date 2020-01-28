6cb38434def736da5a0b1568201540f0359f17de
ivan <irdelgadog@unal.edu.co>
2020-01-28 02:35:00 -0500

    Empelos update

--- teamlinked/src/components/empleos/Empleos.js
+++ teamlinked/src/components/empleos/Empleos.js
@@ -1,48 +1,109 @@
 import React, { Component } from 'react';
 import ItemsCarousel from 'react-items-carousel';
+import {Card, Button} from 'react-bootstrap';
 
 class Empleos extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeItemIndex:0
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItemIndex: 0,
+      empleosPorCategoria: []
+    }
+  }
+  pedirEmpleosPorCategoria(){
+    const url = "http://34.94.208.170:3051/graphql";
+    const query = `
+      query{
+        getEmpleosByCategoria(nombre_categoria:"`+ this.props.categoria + `"){
+          id
+          titulo
+          descripcion
+          fechaPublicacion
+          fechaVencimiento
+          id_ofertante
+          categoria
         }
-    }
+      }
+    `;
+    const opts = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ query })
+    };
 
-    render() {
-        return (
-            <div style={{ "padding": "0 60px", "maxWidth": 800, "margin": "0 auto" }}>
-                <ItemsCarousel
-                  infiniteLoop={false}
-                  gutter={12}
-                  activePosition={'center'}
-                  chevronWidth={60}
-                  disableSwipe={false}
-                  alwaysShowChevrons={false}
-                  numberOfCards={2}
-                  slidesToScroll={2}
-                  outsideChevron={true}
-                  showSlither={false}
-                  firstAndLastGutter={false}
-                  activeItemIndex={this.state.activeItemIndex}
-                  requestToChangeActive={value => this.setState({ activeItemIndex: value })}
-                  rightChevron={'>'}
-                  leftChevron={'<'}
-                >
-                  {Array.from(new Array(10)).map((_, i) =>
-                      <div
-                          key={i}
-                          style={{
-                              height: 200,
-                              background: 'url(https://placeimg.com/380/200/nature)'
-                          }}
-                      />
-                  )}
-                </ItemsCarousel>
-            </div>
-        );
-    }
+    fetch(url, opts)
+      .then(res => res.json())
+      .then(e => {
+        //console.log('RTA',e.data);
+        this.setState({ empleosPorCategoria: e.data.getEmpleosByCategoria })
+        //console.log('RTA2',this.state.empleosPorCategoria);
+        this.forceUpdate();
+      })
+      .catch(console.error);
+
+  }
+
+  componentDidMount(){
+    this.pedirEmpleosPorCategoria();
+  }
+
+
+  render() {
+    if (this.state.empleosPorCategoria.length !== 0){
+    return (
+
+      <div>
+        <h3 style={{ "padding": "0 60px", "maxWidth": 1200, "margin": "15px auto" }}>{this.props.categoria.toUpperCase()}</h3>
+        <div style={{ "padding": "0 60px", "maxWidth": 1200, "margin": "10px auto" }}>
+          <ItemsCarousel
+            infiniteLoop={false}
+            gutter={12}
+            activePosition={'center'}
+            chevronWidth={60}
+            disableSwipe={false}
+            alwaysShowChevrons={false}
+            numberOfCards={4}
+            slidesToScroll={1}
+            outsideChevron={true}
+            showSlither={false}
+            firstAndLastGutter={false}
+            activeItemIndex={this.state.activeItemIndex}
+            requestToChangeActive={value => this.setState({ activeItemIndex: value })}
+            
+            rightChevron={<button>{'>'}</button>}
+            leftChevron={<button>{'<'}</button>}
+          >
+            {Array.from(this.state.empleosPorCategoria).map((_, i) =>
+              <div
+                key={i}
+              >
+                <Card style={{ width: '16rem', height: '16rem' }}>
+                  
+                  <Card.Body>
+            <Card.Title>{this.state.empleosPorCategoria[i].titulo}</Card.Title>
+                    <Card.Text>
+                      - Publicado en: {this.state.empleosPorCategoria[i].fechaPublicacion} <br></br>
+                      - Vence en: {this.state.empleosPorCategoria[i].fechaVencimiento} 
+                    </Card.Text>
+                    <Button variant="primary">Ver mas</Button>
+                  </Card.Body>
+                </Card>
+
+              </div>
+            )}
+          </ItemsCarousel>
+        </div>
+      </div>
+    );
+  }
+  return(
+    <div></div>
+  );
+  }
 }
 
 export default Empleos;
